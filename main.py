9808de8dc4ac0ad19bfff6b48d3baeed8ca13fde
Kiran Irde <kbirde@umich.edu>
2024-03-16 00:02:17 +0000

    Cleaned files with pylint
    Changes to be committed:
    	modified:   codes.py
    	modified:   main.py
    	modified:   map_donut.py
    	modified:   state_dict.py

--- main.py
+++ main.py
@@ -1,34 +1,12 @@
 """ This is the main file to run all the other modules"""
 import argparse
 from map_donut import map_donut
-from bar_plot import bar_plot
-from codes import codes
 
-# def main():
-#     "Main function that gets users state id code input and uses it for plotting information"
-    
-#     # Get user input
-#     print('----------------------------------------------------------')
-#     user_input = int(input("Enter state code id: "))
-
-#     # Call the map_donut function with user input
-#     map_donut(user_input)
-
-#     # Call the bar_plot function with user input
-#     bar_plot(user_input)
-
-# if __name__ == "__main__":
-#     codes()
-#     main()
-
-def main(accident_file, input_state1, year1, person_file, input_state2, year2):
+def main(accident_file, input_state, year):
     "Main function that gets users state id code input and uses it for plotting information"
 
     # Call the map_donut function with user input
-    map_donut(accident_file, input_state1, year1)
-
-    # Call the bar_plot function with user input
-    bar_plot(person_file, input_state2, year2)
+    map_donut(accident_file, input_state, year)
 
 if __name__ == "__main__":
 
@@ -36,11 +14,8 @@ if __name__ == "__main__":
         description="Bar plot and Map / Donut chart generator"
         )
     parser.add_argument("accident_file", help="Path to ACCIDENT_YEAR.csv file")
-    parser.add_argument("input_state1", help="State code ID ( Use: python3 codes.py)")
-    parser.add_argument("year1", help="Enter year of interest")
-    parser.add_argument("person_file", help="Path to PERSON_YEAR.csv file")
-    parser.add_argument("input_state2", help="State code ID ( Use: python3 codes.py)")
-    parser.add_argument("year2", help="Enter year of interest")
+    parser.add_argument("input_state", help="State code ID ( Use: python3 codes.py)")
+    parser.add_argument("year", help="Enter year of interest")
     args = parser.parse_args()
 
-    main(args.accident_file, args.input_state1, args.year1, args.person_file, args.input_state2, args.year2)
+    main(args.accident_file, args.input_state, args.year)
--- map_donut.py
+++ map_donut.py
@@ -1,43 +1,46 @@
-"""This module is for plotting US statewise accident location map & a Donut chart for accidents occurring on different days of the week """
+"""This module is for plotting US statewise accident location map & a Donut chart
+for accidents occurring on different days of the week """
 import argparse
 import pandas as pd
-import numpy as np
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 from state_dict import state_dict
 
-def map_donut(accident_file , input_state, year2):
+def map_donut(accident_file , input_state, year):
     """This function takes in user input and plots the map & donut chart"""
 
     # Assign the state_dictionary
     state_dict1 = state_dict
 
-    # Setting up try block of statements that will work if existing state code is entered 
+    # Setting up try block of statements that will work if existing state code is entered
     try:
 
-        # Assigning the state input from the user    
+        # Assigning the state input from the user
         get_state = state_dict1[int(input_state)]
         get_state_id = int(input_state)
 
-        # Reading in 2010 ACCIDENT data, downselecting the rows related to the user's state choice & reducing the dataframe to 4 columns
+        # Reading in 2010 ACCIDENT data, downselecting the rows related to the user's state
+        # choice & reducing the dataframe to 4 columns
         acc_data = pd.read_csv(accident_file, encoding='utf-8')
         state_data = acc_data[acc_data.STATE ==get_state_id]
         state_data = state_data.loc[:,['DAY_WEEK','HOUR','LATITUDE','LONGITUD']]
         state_accident_count = state_data.groupby(['DAY_WEEK']).count()
         state_accident_count_list = state_accident_count['HOUR'].tolist()
 
-        # Setting up a subplot with 1 row and 2 columns, specifying that the first column is a scattergeo map and the second column is a pie chart
+        # Setting up a subplot with 1 row and 2 columns, specifying that the first column
+        # is a scattergeo map and the second column is a pie chart
         fig = make_subplots(rows=1, cols=2,
                             column_widths=[0.6,0.4],
                             row_heights=[0.4],
                             specs=[[{"type": "scattergeo"}, {"type": "pie"}]])
 
-    # Adding a Scattergeo trace with latitude, longitude, marker (w/ color), title & focusing map to USA region
+    # Adding a Scattergeo trace with latitude, longitude, marker (w/ color), title &
+    # focusing map to USA region
         fig.add_trace(
             go.Scattergeo(lat=state_data["LATITUDE"],
                         lon=state_data["LONGITUD"],
                         mode="markers",
-                        marker= dict(color='red'),
+                        marker= {"color":'red'},
                         name='Accident Location',
                         locationmode = 'USA-states',)
         )
@@ -49,13 +52,15 @@ def map_donut(accident_file , input_state, year2):
         labels = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
         fig.add_trace(go.Pie(labels=labels, values=state_accident_count_list, hole=.6),row=1, col=2)
 
-    # Updating figure layout with different titles, NOTE: the main title dynamically updates the state name as selected by the user
+    # Updating figure layout with different titles
+    # NOTE: the main title dynamically updates the
+    # state name as selected by the user
         fig.update_layout(
             geo_scope='usa',
             height = 500,
             width = 900,
             title={
-                'text': "<b> Accident Analysis for {} ({})".format(get_state, year2),
+                'text': f"<b> Accident Analysis for {get_state} ({year})",
                 'y':0.9,
                 'x':0.5,
                 'xanchor': 'center',
@@ -87,9 +92,9 @@ def map_donut(accident_file , input_state, year2):
         map_donut_fig = fig.show()
         return map_donut_fig
 
-    # Setting up except block of statements that will work if existing state code is NOT entered 
-    except:
-        print("State information is not available")
+    # Setting up except block of statements that will work if existing state code is NOT entered
+    except KeyError:
+        return print("State information is not available")
 
 if __name__ == '__main__':
 
@@ -98,7 +103,7 @@ if __name__ == '__main__':
         )
     parser.add_argument("accident_file", help="Path to ACCIDENT_YEAR.csv file")
     parser.add_argument("input_state", help="State code ID ( Use: python3 codes.py)")
-    parser.add_argument("year2", help="Enter year of interest") 
+    parser.add_argument("year", help="Enter year of interest")
     args = parser.parse_args()
 
-    map_donut(args.accident_file, args.input_state, args.year2)
+    map_donut(args.accident_file, args.input_state, args.year)
